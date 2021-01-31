03f41542cc724488e4ab1f48b82670650c68f517
easley <synchronizedlock@gmail.com>
2021-01-31 10:02:42 +0800

    nami-server:核心结构

---
+++ Week_03/nami/nami-server/src/main/java/org/nami/NamiServerApplication.java
@@ -0,0 +1,12 @@
+package org.nami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamiServerApplication.class);
+    }
+}
